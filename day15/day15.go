70ee665d7b41c594fee7f540423b28d7db0c9ab9
Gregory McWhirter <gregm@ziprecruiter.com>
2016-12-16 12:31:01 -0800

    Day 15

--- day15/day15.go
+++ day15/day15.go
@@ -1,9 +1,84 @@
 package day15
 
-func RunPartA(filename string) {
+import (
+	"fmt"
+	"strings"
 
+	"regexp"
+	"strconv"
+
+	"github.com/gsmcwhirter/advent2016/lib"
+)
+
+type Disc struct {
+	NumSlots    int
+	CurrentSlot int
+}
+
+func (d *Disc) SlotAfterRotations(steps int) int {
+	return (d.CurrentSlot + steps) % d.NumSlots
+}
+
+type State struct {
+	Discs []Disc
+}
+
+func (s *State) DropSuccessful(time int) bool {
+	for i, disc := range s.Discs {
+		if disc.SlotAfterRotations(time+i+1) > 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func ParseDataString(dat string) State {
+	discs := []Disc{}
+
+	r := regexp.MustCompile("has (\\d+) positions; at time=0, it is at position (\\d+)")
+	for _, line := range strings.Split(dat, "\n") {
+		matches := r.FindStringSubmatch(line)
+		numSlots, _ := strconv.Atoi(matches[1])
+		pos, _ := strconv.Atoi(matches[2])
+		discs = append(discs, Disc{
+			NumSlots:    numSlots,
+			CurrentSlot: pos,
+		})
+	}
+
+	return State{discs}
+}
+
+func LoadData(filename string) State {
+	dat := lib.ReadFileData(filename)
+
+	return ParseDataString(strings.Trim(string(dat), "\n"))
+}
+
+func RunPartA(filename string) {
+	state := LoadData(filename)
+
+	time := 0
+	for {
+		if state.DropSuccessful(time) {
+			fmt.Println(time)
+			break
+		}
+		time++
+	}
 }
 
 func RunPartB(filename string) {
+	state := LoadData(filename)
 
+	time := 0
+	for {
+		if state.DropSuccessful(time) {
+			fmt.Println(time)
+			break
+		}
+
+		time++
+	}
 }
